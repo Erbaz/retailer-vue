bb05900b5cfbda5e7795f55fcfc9843bf3a50bc6
merbaz <merbaz@nisum.com>
2025-02-05 21:30:08 +0500

    simple starter sample to use web components with

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,34 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <HelloWorld msg="Welcome to Your Vue.js App" />
+  <button @click="toggleSmartApply">
+    {{ showSmartApply ? "Hide" : "Show" }} Smart Apply
+  </button>
+
+  <div v-if="showSmartApply" class="smart-apply" style="">
+    <smart-apply />
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from "./components/HelloWorld.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    HelloWorld,
+  },
+  data() {
+    return {
+      showSmartApply: false, // State variable to track visibility
+    };
+  },
+  methods: {
+    toggleSmartApply() {
+      this.showSmartApply = !this.showSmartApply;
+    },
+  },
+};
 </script>
 
 <style>
@@ -22,5 +39,16 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+.smart-apply {
+  width: 30%;
+  border: 1px solid black;
+  background-color: white;
+  position: absolute;
+  padding: 10px;
+  top: 0;
+  left: 0;
+  right: 0;
+  margin: 0;
 }
 </style>
